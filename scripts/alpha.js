da62e0b929470849f039ebe80c5009cf82ffe8a8
shohag0007 <merinedrive007@gmail.com>
2024-02-24 14:46:10 +0600

    alpha clash is working

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -6,11 +6,34 @@
 //     playGroundSection.classList.remove('hidden');
 // }
 
+function handleKeyBoardKeyUpEvent(event){
+ 
+    const playerPressed = event.key;
+    console.log('Player Pressed',playerPressed);
+// key player is  expected to press.
+const currentAlphabetElement = document.getElementById('current-alphabet');
+const currentAlphabet = currentAlphabetElement.innerText;
+const expectedAlphabet = currentAlphabet.toLowerCase();
+console.log(playerPressed, currentAlphabet);
+// check right or wrong key pressed
+if(playerPressed === expectedAlphabet){
+    console.log('you get a point');
+    console.log('You have pressed correctly', expectedAlphabet);
+    removeBackgroundColorById(expectedAlphabet);
+    continueGame();
+}
+else{
+    console.log('you missed. you lost a life');
+}
+}
+
+// capture keyboard key press
+document.addEventListener('keyup',handleKeyBoardKeyUpEvent);
 
 function continueGame(){
 // step-1 generate a random alphabet
 const alphabet = getARandomAlphabet();
-console.log('Your Random Alphabet',alphabet);
+//console.log('Your Random Alphabet',alphabet);
 
 // set randomly generated alphabet to the screen. (show it)
 const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -24,5 +47,5 @@ setBackgroundColorById(alphabet);
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
-    continueGame()
+    continueGame();
 }
